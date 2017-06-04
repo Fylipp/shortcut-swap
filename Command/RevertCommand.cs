d7425c7c05452c1a39c2475a10ce185e03e8936b
Philipp Ploder <philiplo@live.at>
2017-06-04 15:37:06 +0200

    Add swap and revert logic

--- Command/RevertCommand.cs
+++ Command/RevertCommand.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Mono.Options;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ShortcutSwap.Command {
     class RevertCommand : Mono.Options.Command {
@@ -11,10 +14,10 @@ namespace ShortcutSwap.Command {
         public string Root { get; private set; } = ".";
 
         public RevertCommand() : base("revert", "Revert a swap") {
-            Options = new Mono.Options.OptionSet {
+            Options = new OptionSet {
                 { "help", "Shows help", h => ShowHelp = true, true },
-                { "verbose", "Whether the progress should be logged (dramatically increases duration)", v => Verbose = true },
-                { "root", "The root of the folder hierarchy at which the program should start", r => Root = r },
+                { "verbose|v", "Logs all progress", v => Verbose = true },
+                { "root=", "The root of the folder hierarchy at which the program should start", r => Root = r },
             };
         }
 
@@ -25,13 +28,51 @@ namespace ShortcutSwap.Command {
                 if (ShowHelp) {
                     Options.WriteOptionDescriptions(CommandSet.Out);
                 } else {
-                    // TODO implement reverting
+                    var rfPath = Path.Combine(Root, Program.RevertFile);
+                    var rawRevertInfo = File.ReadAllText(rfPath);
+                    var revertInfo = JsonConvert.DeserializeObject<Dictionary<string, string>>(rawRevertInfo);
+
+                    var shell = new IWshRuntimeLibrary.WshShell();
+
+                    var failed = new Dictionary<string, string>();
+
+                    foreach (var pair in revertInfo) {
+                        if (!Revert(pair.Key, pair.Value, shell)) {
+                            failed.Add(pair.Key, pair.Value);
+                        }
+                    }
+
+                    if (failed.Count == 0) {
+                        Console.WriteLine($"Successfully reverted {revertInfo.Count} shortcuts, backup will be removed.");
+                        File.Delete(rfPath);
+                    } else {
+                        Console.WriteLine($"Failed to revert {failed.Count} of {revertInfo.Count} shortcuts, backup will be updated.");
+                        File.WriteAllText(rfPath, JsonConvert.SerializeObject(failed));
+                    }
                 }
 
                 return 0;
             } catch (Exception e) {
-                Console.Error.WriteLine("Uncaught exception: " + e.Message);
+                Console.Error.WriteLine("[ERROR] " + e.Message);
                 return 1;
+            }
+        }
+
+        bool Revert(string filepath, string oldTarget, IWshRuntimeLibrary.WshShell shell) {
+            try {
+                var link = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(filepath);
+
+                link.TargetPath = oldTarget;
+                link.Save();
+
+                if (Verbose) {
+                    Console.WriteLine($"[REVERT] {filepath}");
+                }
+
+                return true;
+            } catch (Exception e) {
+                Console.WriteLine($"[FAIL] {filepath} -> {oldTarget}{Environment.NewLine}       {e.Message}");
+                return false;
             }
         }
 
--- Command/SwapCommand.cs
+++ Command/SwapCommand.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Mono.Options;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ShortcutSwap.Command {
     class SwapCommand : Mono.Options.Command {
@@ -10,17 +13,17 @@ namespace ShortcutSwap.Command {
 
         public bool Verbose { get; private set; } = false;
 
-        public string Root { get; private set; } = ".";
+        public string Root { get; private set; } = Directory.GetCurrentDirectory();
 
         public int DepthLimit { get; private set; } = 2;
 
         public SwapCommand() : base("swap", "Perform a swap") {
-            Options = new Mono.Options.OptionSet {
-                { "link", "The custom shortcut to change all .lnk-files to", l => Link = l },
-                { "help", "Shows help", h => ShowHelp = true, true },
-                { "verbose", "Whether the progress should be logged (dramatically increases duration)", v => Verbose = true },
-                { "root", "The root of the folder hierarchy at which the program should start", r => Root = r },
-                { "depth-limit", "The maximum hierarchy depth", (int d) => DepthLimit = d }
+            Options = new OptionSet {
+                { "link|l=", "The custom shortcut to change all .lnk-files to", l => Link = l },
+                { "help|h", "Shows help", h => ShowHelp = true, true },
+                { "verbose|v", "Logs all progress", v => Verbose = true },
+                { "root|r=", "The root of the folder hierarchy at which the program should start", r => Root = r },
+                { "depth-limit|d=", "The maximum hierarchy depth", (int d) => DepthLimit = d }
             };
         }
 
@@ -31,13 +34,83 @@ namespace ShortcutSwap.Command {
                 if (ShowHelp) {
                     Options.WriteOptionDescriptions(CommandSet.Out);
                 } else {
-                    // TODO implement swapping
+                    if (string.IsNullOrWhiteSpace(Link)) {
+                        Console.Error.WriteLine("Non-whitespace link is mandatory.");
+                        return 1;
+                    }
+
+                    var rfPath = Path.Combine(Root, Program.RevertFile);
+
+                    if (File.Exists(rfPath)) {
+                        Console.Error.WriteLine("The path for revert information already exists. Revert before swapping again.");
+                        return 1;
+                    }
+
+                    var rfStream = new StreamWriter(new FileStream(rfPath, FileMode.Create));
+                    var revertInfo = new Dictionary<string, string>();
+
+                    var shell = new IWshRuntimeLibrary.WshShell();
+
+                    int success = 0;
+                    int total = 0;
+
+                    Walk(Root, 0, filename => {
+                        if (Swap(filename, revertInfo, shell)) {
+                            success++;
+                        }
+
+                        total++;
+                    });
+
+                    rfStream.Write(JsonConvert.SerializeObject(revertInfo));
+                    rfStream.Close();
+
+                    File.SetAttributes(rfPath, File.GetAttributes(rfPath) | FileAttributes.Hidden);
+
+                    Console.WriteLine($"Swapped {success} of {total} shortcuts.");
                 }
 
                 return 0;
             } catch (Exception e) {
-                Console.Error.WriteLine("Uncaught exception: " + e.Message);
+                Console.Error.WriteLine($"[ERROR] {e.Message}");
                 return 1;
+            }
+        }
+
+        bool Swap(string filepath, IDictionary<string, string> revertInfo, IWshRuntimeLibrary.WshShell shell) {
+            try {
+                var link = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(filepath);
+
+                var oldTarget = link.TargetPath;
+
+                link.TargetPath = Link;
+                link.IconLocation = oldTarget;
+                link.Save();
+
+                revertInfo.Add(filepath, oldTarget);
+
+                if (Verbose) {
+                    Console.WriteLine($"[SWAP] {filepath}");
+                }
+
+                return true;
+            } catch (Exception e) {
+                Console.WriteLine($"[FAIL] {filepath}{Environment.NewLine}       {e.Message}");
+                return false;
+            }
+        }
+
+        void Walk(string directory, int currentDepth, Action<string> processor) {
+            if (currentDepth >= DepthLimit) {
+                return;
+            }
+
+            foreach (string shortcutFile in Directory.GetFiles(directory, "*.lnk")) {
+                processor(shortcutFile);
+            }
+
+            foreach (string subDirectory in Directory.GetDirectories(directory)) {
+                Walk(subDirectory, currentDepth + 1, processor);
             }
         }
 
